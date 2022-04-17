974bbc0660691c4d434736c897cf7a1f1ef555f8
marieooq <otakimarie@gmail.com>
2022-04-17 11:50:10 -0700

    connect mysql

--- server/index.js
+++ server/index.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+require("dotenv").config();
+
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: "crud",
+});
 
 app.get("/", (req, res) => {
-  res.send("Hello Marie");
+  const sqlInsert =
+    "INSERT INTO movie_reviews (movieName, movieReview) VALUES ('Asakusa kid', 'it reminds of Japanese Showa era');";
+  console.log("process.env.DB_HOST", process.env.DB_HOST);
+  db.query(sqlInsert, (err, result) => {
+    res.send("Hello Marie");
+    // console.log(err);
+  });
 });
 
 app.listen("3001", () => {
